4aad883cd5b2faac7e8942630938d96f6e71b5ee
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-04-22 11:47:31 +0600

    admin home setup

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,10 +1,61 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const AdminHome = () => {
-    return (
-        <div>
-            
+  const axiosSecure = useAxiosSecure();
+
+  const { data } = useQuery({
+    queryKey: ["data"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
+
+  return (
+    <div>
+      <div className="bg-gray-900 sm:py-10">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:max-w-none">
+            <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+              <div className="flex flex-col bg-white/5 p-8">
+                <dt className="text-sm font-semibold leading-6 text-gray-300">
+                  Users
+                </dt>
+                <dd className="order-first text-3xl font-semibold tracking-tight text-white">
+                  {data?.user}
+                </dd>
+              </div>
+              <div className="flex flex-col bg-white/5 p-8">
+                <dt className="text-sm font-semibold leading-6 text-gray-300">
+                  Room
+                </dt>
+                <dd className="order-first text-3xl font-semibold tracking-tight text-white">
+                  {data?.room}
+                </dd>
+              </div>
+              <div className="flex flex-col bg-white/5 p-8">
+                <dt className="text-sm font-semibold leading-6 text-gray-300">
+                  Order
+                </dt>
+                <dd className="order-first text-3xl font-semibold tracking-tight text-white">
+                  {data?.order}
+                </dd>
+              </div>
+              <div className="flex flex-col bg-white/5 p-8">
+                <dt className="text-sm font-semibold leading-6 text-gray-300">
+                  Revenue
+                </dt>
+                <dd className="order-first text-3xl font-semibold tracking-tight text-white">
+                  ${data?.revenue}
+                </dd>
+              </div>
+            </dl>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
